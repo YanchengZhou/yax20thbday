cfdf8556128cd63484cbcef638e369bae8c85d4b
YanchengZhou <yanchengzhou020830@gmail.com>
2022-06-23 12:46:00 -0400

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+          style={{
+            width: "500px",
+            height: "300px",
+            overflowY: "scroll",
+            backgroundColor: "#e8fffc",
+            paddingLeft: "5px",
+            paddingRight: "10px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h2>
+            <u>Read Notifications</u>
+          </h2>
+         
+        </div>
   );
 }
 
